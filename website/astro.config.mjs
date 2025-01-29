abedfa886ee5f970db5594157c3990d4985aec18
Philip Miglinci <pmig@glasskube.com>
2025-01-29 15:29:58 +0100

    docs(website): add cta buttons

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -17,6 +17,20 @@ export default defineConfig({
           tag: 'script',
           content: `window.addEventListener('load', () => document.querySelector('.site-title').href += 'docs/')`,
         },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'preconnect',
+            href: 'https://p.glasskube.eu',
+          },
+        },
+        {
+          tag: 'script',
+          content: `
+            !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys getNextSurveyStep onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+            posthog.init( 'phc_EloQUW6cgfbTc0pI9c5CXElhQ4gVGRoBsrUAoakJVoQ', { api_host: 'https://p.glasskube.eu', ui_host: 'https://eu.posthog.com', } )
+          `,
+        },
       ],
       description: 'Open Source Software Distribution Platform',
       logo: {
@@ -25,6 +39,10 @@ export default defineConfig({
       social: {
         github: 'https://github.com/glasskube/distr',
         discord: 'https://discord.gg/6qqBSAWZfW',
+      },
+      components: {
+        // Override the default `SocialIcons` component.
+        SocialIcons: './src/components/NavBarCta.astro',
       },
       sidebar: [
         {
