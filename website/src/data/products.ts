fe0a3e80b73377402f99b5cb964bc40a62a009fc
Philip Miglinci <pmig@glasskube.com>
2026-02-17 17:31:52 +0100

    docs(website): pre flight check documentation

--- website/src/data/products.ts
+++ website/src/data/products.ts
@@ -49,11 +49,11 @@ export const products: Product[] = [
     href: '/docs/guides/kubernetes-compatibility-matrix/',
   },
   {
-    title: 'Pre Flight Checks',
+    title: 'Pre-flight Checks',
     description:
-      'In Development - Determine if needed resources are available or execute custom pre and post installation scripts',
+      'New - Determine if needed resources are available or execute custom pre and post installation scripts',
     value: 'clipboard-check',
-    href: '/docs/product/agents/',
+    href: '/docs/guides/preflight-checks/',
   },
   {
     title: 'Customer Portal',
